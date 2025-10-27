cac45b99a3f6573e97bdbf4a64e9c0c954a5c9b9
okotekkws <159702336+okotekkws@users.noreply.github.com>
2025-10-27 12:28:02 +0100

    Update szoty.js

--- szoty.js
+++ szoty.js
@@ -1,7 +1,12 @@
 export const szoty = [
     {
-        "url": "O Kotek URL",
-        "title": "O Kotek Title",
-        "description": "O Kotek Description"
+        "url": "https://streamable.com/ev94g3",
+        "title": "cip-cip-cip-kurwa-koniec-strima-okotek",
+        "description": "Majus kończy strima i idzie spać"
+    },
+    {
+        "url": "https://streamable.com/hnddwu",
+        "title": "majus-barber-okotek",
+        "description": "Majus się goli wyłączoną maszynką elektryczną"
     }
 ]
